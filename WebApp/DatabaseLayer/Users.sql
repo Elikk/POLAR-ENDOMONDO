9b49f1ca022553916c0644d4aef3ba67c5da15fe
Elikk <Elijus.Sekonas@gmail.com>
2016-05-02 18:11:07 +0300

    DBBB

--- WebApp/DatabaseLayer/Users.sql
+++ WebApp/DatabaseLayer/Users.sql
@@ -1,4 +1,8 @@
 ﻿CREATE TABLE [dbo].[Users]
 (
-	[Id] INT NOT NULL PRIMARY KEY
+	[Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY , 
+    [UserName] NVARCHAR(50) NOT NULL, 
+    [UserPassword] NVARCHAR(50) NOT NULL, 
+    [UserRights] BINARY(50) NOT NULL, 
+    [LastLogin] TIMESTAMP NULL
 )
